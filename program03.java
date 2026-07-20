315fa0ec2ba67b8881b00ba8af964d9da446b882
Lovekush Gour <lovekushgour686@gmail.com>
2026-07-20 11:31:20 +0530

    Add files via upload

---
+++ program03.java
@@ -0,0 +1,17 @@
+/*3. Addition of Two Numbers
+Question
+
+Write a Java program to add two numbers and display the result.
+
+Example
+10 + 20 = 30 */
+public class program03 {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        System.out.println("Sum of Two num:: ---->");
+        System.out.println(sum);
+    }
+
+}
---
+++ program04.java
@@ -0,0 +1,12 @@
+/*Question
+
+Write a Java program to find the area of a rectangle. */
+public class program04 {
+    public static void main(String[] args) {
+        int len = 10;
+        int wid = 20;
+        int Area = len * wid;
+        System.out.println("Area of Rectangle ==> ");
+        System.out.println(len + " * " + wid + " = " + Area);
+    }
+}
